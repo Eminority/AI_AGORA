5c31a71e753fe7563f3efc893d513aa3be1351f8
Nayuz <nayuz.nil@gmail.com>
2025-02-11 17:28:06 +0900

    autodebate

--- webserver/router/lobby.py
+++ webserver/router/lobby.py
@@ -8,4 +8,8 @@ templates = Jinja2Templates(directory="templates")
 
 @router.get("/lobby")
 async def lobby(request: Request):
-    return templates.TemplateResponse("lobby.html", {"request": request, "title":"TITLE TEST"})
+    return templates.TemplateResponse("lobby.html", {"request": request})
+
+@router.get("/lobby/create")
+async def lobby_create_debate(request:Request):
+    return templates.TemplateResponse("lobby_create_profile.html", {"request":request})
