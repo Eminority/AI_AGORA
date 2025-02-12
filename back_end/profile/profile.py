8e4d63c3b917dc25b38929ce4fd1121366fbd908
Nayuz <nayuz.nil@gmail.com>
2025-02-12 11:51:38 +0900

    profile

---
+++ back_end/profile/profile.py
@@ -0,0 +1,56 @@
+## ai 프로필을 만들어서 저장하고 불러올 수 있게끔 하는 모듈
+from db_module import MongoDBConnection
+
+class ProfileManager:
+    def __init__(self, db: MongoDBConnection):
+        data = db.select_data_from_query(collection_name="object")
+        self.objectlist = {}
+        for raw_object in data:
+            obj = Profile(_id               = raw_object.get("_id"),
+                          name              = raw_object.get("name"),
+                          img               = raw_object.get("img"),
+                          ai                = raw_object.get("ai"),
+                          create_date       = raw_object.get("create_date"),
+                          object_attribute  = raw_object.get("object_attribute"),
+                          debate_history    = raw_object.get("debate_history", [])
+                          )
+            self.objectlist[obj._id] = obj
+    
+    def create_profile(self,
+                        name:str=None,
+                        img:str=None,
+                        ai:str=None,
+                        create_date:str=None,
+                        object_attribute:str=None):
+        new_obj = Profile(name=name,
+                            img=img,
+                            ai=ai,
+                            create_date=create_date,
+                            object_attribute=object_attribute)
+        new_obj.save(self.db)
+        self.objectlist[new_obj.data["_id"]] = new_obj
+
+    
+class Profile:
+    def __init__(self, _id:str=None,
+                        name:str=None,
+                        img:str=None,
+                        ai:str=None,
+                        create_date:str=None,
+                        object_attribute:str=None,
+                        debate_history:list=[]):
+        self.data = {}
+        self.data["name"]           = name
+        self.data["img"]            = img
+        self.data["ai"]             = ai
+        self.data["create_date"]    = create_date
+        self.data["object_attribute"] = object_attribute
+        self.data["debate_history"] = debate_history
+        if _id: # id가 None으로라도 들어가있으면 에러 내니까 None이면 아예 안만들기
+            self.data["_id"]        = _id
+
+    def save(self, db:MongoDBConnection):
+        if self.data.get("_id"): #_id가 이미 있는 경우 update
+            db.update_data("object", self.data)
+        else : #_id가 없는 경우 insert하고 self id저장
+            self.data["_id"] = db.insert_data("object", self.data)
