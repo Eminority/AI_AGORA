211eeb8eff9ad11a4ec52c7ea9e536850d8a91c0
Nayuz <nayuz.nil@gmail.com>
2025-02-12 11:32:34 +0900

    select_data_from_query

--- back_end/db_module.py
+++ back_end/db_module.py
@@ -24,9 +24,13 @@ class MongoDBConnection:
     #RDBMS에서의 insert 문을 대체
     def insert_data(self, collection_name: str, data:dict):
         return self.db[collection_name].insert_one(data).inserted_id
-    #RDBMS 쿼리문에서의 Select문을 대체.
+    #RDBMS 쿼리문에서의 Select문을 대체 - id로 선택.
     def select_data_from_id(self, collection_name: str, id:str):
         return self.db[collection_name].find_one({"_id":ObjectId(id)})
+
+    #RDBMS 쿼리문에서의 Select문을 대체 - 쿼리문 작성 필요. 비어있으면 컬렉션 전체 가져옴
+    def select_data_from_query(self, collection_name:str, query:dict={}):
+        return self.db[collection_name].find(query)
 
     #RDBMS 쿼리문에서의 Update문을 대체.
     def update_data(self, collection_name: str, data:dict):
